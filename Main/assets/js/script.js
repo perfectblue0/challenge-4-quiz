8b18f21bbec2a90301d1bc73965eb8b10779a138
Paola_G <perfectblue@live.com>
2023-04-15 16:24:57 -0700

    added quiz questions to JS file

---
+++ Main/assets/js/script.js
@@ -0,0 +1,53 @@
+const questions = [
+    {
+        question: "What is the capital of Aguascalientes, Mexico?",
+        answers: {
+            1: "Saltillo",
+            2: "Tijuana",
+            3: "Guanajuato",
+            4: "Aguascalientes",
+        },
+        correctAnswer: "4"
+    },
+    {
+        question: "What is the capital of Durango?",
+        answers: {
+            1: "Colima",
+            2: "Victoria de Durango",
+            3: "Guadalajara",
+            4: "Morelia",
+        },
+        correctAnswer: "2"
+    },
+    {
+        question: "What is the capital of Nayarit?",
+        answers: {
+            1: "Pachuca de Soto",
+            2: "Mexico City",
+            3: "Cuernavaca",
+            4: "Tepic",
+        },
+        correctAnswer: "4"
+    },
+    {
+        question: "What is the capital of Tabasco",
+        answers: {
+            1: "Ciudad Juarez",
+            2: "Hermosillo",
+            3: "Monterrey",
+            4: "Oaxaca de Juárez",
+        },
+        correctAnswer: "2"
+    },
+    {
+        question: "What is the capital of Zacatecas",
+        answers: {
+            1: "Zacatecas",
+            2: "La Paz",
+            3: "Manzanillo",
+            4: "Tijuana",
+        },
+        correctAnswer: "1"
+    },
+
+];
