456509a3e276f825d92b57a34b6d368dea222cf6
Paola_G <perfectblue@live.com>
2023-04-17 11:45:57 -0700

    changed format of questions so I can work with it's index number in loadQuestions function

--- Main/assets/js/script.js
+++ Main/assets/js/script.js
@@ -1,53 +1,53 @@
 const questions = [
     {
         question: "What is the capital of Aguascalientes, Mexico?",
-        answers: {
-            1: "Saltillo",
-            2: "Tijuana",
-            3: "Guanajuato",
-            4: "Aguascalientes",
-        },
-        correctAnswer: "4"
+        answers: [
+            "Saltillo",
+            "Tijuana",
+            "Guanajuato",
+            "Aguascalientes",
+        ],
+        correctAnswer: 3
     },
     {
         question: "What is the capital of Durango?",
-        answers: {
-            1: "Colima",
-            2: "Victoria de Durango",
-            3: "Guadalajara",
-            4: "Morelia",
-        },
-        correctAnswer: "2"
+        answers: [
+            "Colima",
+            "Victoria de Durango",
+            "Guadalajara",
+            "Morelia",
+        ],
+        correctAnswer: 1
     },
     {
         question: "What is the capital of Nayarit?",
-        answers: {
-            1: "Pachuca de Soto",
-            2: "Mexico City",
-            3: "Cuernavaca",
-            4: "Tepic",
-        },
-        correctAnswer: "4"
+        answers: [
+            "Pachuca de Soto",
+            "Mexico City",
+            "Cuernavaca",
+            "Tepic",
+        ],
+        correctAnswer: 3
     },
     {
         question: "What is the capital of Tabasco",
-        answers: {
-            1: "Ciudad Juarez",
-            2: "Hermosillo",
-            3: "Monterrey",
-            4: "Oaxaca de Juárez",
-        },
-        correctAnswer: "2"
+        answers: [
+            "Ciudad Juarez",
+            "Hermosillo",
+            "Monterrey",
+            "Oaxaca de Juárez",
+        ],
+        correctAnswer: 1
     },
     {
         question: "What is the capital of Zacatecas",
-        answers: {
-            1: "Zacatecas",
-            2: "La Paz",
-            3: "Manzanillo",
-            4: "Tijuana",
-        },
-        correctAnswer: "1"
+        answers: [
+            "Zacatecas",
+            "La Paz",
+            "Manzanillo",
+            "Tijuana",
+        ],
+        correctAnswer: 0
     },
 
 ];
@@ -72,6 +72,12 @@ function gameStart() {
 }
 
 // To do: add function declaration that decreases time
+
+function loadQuestions() {
+    var question = questions[]
+}
+
+// function that hides elements and shows others on startbutton click
 
 startButton.addEventListener("click", function() {
     document.querySelector("#box-1").setAttribute("class", "hide");
